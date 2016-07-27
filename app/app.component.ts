92c4a8ee34e76ba0437d3c1716989645a966c6e3
JessWhite <jessica.white@oakbrookfinance.com>
2016-07-27 12:00:30 +0100

    little text edit to page. learn about interpolation binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    <h1>{{title}}</h1>
+    <div [style.color] = "wardsColor">
+        Hello {{name}}
+    </div>
+    `
 })
-export class AppComponent { }
+export class AppComponent { 
+    title = 'Customer App';
+    name = 'Superman';
+    wardsColor ='blue';
+}
